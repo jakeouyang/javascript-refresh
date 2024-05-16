fad722b9d4700e1a5a13d840aea1819131133364
Jake OuYang <jakeouyang@hotmail.com>
2024-05-16 10:21:51 +0800

    update

---
+++ assets/scripts/app.js
@@ -0,0 +1,12 @@
+// import { apiKey } from './util.js';
+
+// console.log(apiKey);
+
+const hobbies = ["Sports", "Cooking", "Reading"];
+
+const index = hobbies.findIndex((item) => {
+  return item === 'Reading';
+});
+
+console.log(index)
+
